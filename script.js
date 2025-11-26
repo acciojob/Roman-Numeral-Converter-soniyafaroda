4995e567c1556a6d0c192087a540ddcfb6f0ae2b
Soniya Faroda <143260442+soniyafaroda@users.noreply.github.com>
2025-11-26 23:29:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,12 +12,36 @@ function convertToRoman(num) {
   //your code here
 
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+  let roman = "";
 
-// console.log(convertToRoman(36));
+  // Loop through the object values
+  for (let i = 0; i <= 6; i++) {
+    const symbol = obj[i][0];
+    const value = obj[i][1];
 
+    // Add the symbol while num is greater than or equal to value
+    while (num >= value) {
+      roman += symbol;
+      num -= value;
+    }
+  }
 
+  // Handle subtractive notation replacements
+  roman = roman
+    .replace(/DCCCC/g, "CM") // 900
+    .replace(/CCCC/g, "CD")  // 400
+    .replace(/LXXXX/g, "XC") // 90
+    .replace(/XXXX/g, "XL")  // 40
+    .replace(/VIIII/g, "IX") // 9
+    .replace(/IIII/g, "IV"); // 4
 
+  return roman;
+}
+
+// Example tests:
+// console.log(convertToRoman(14));   // XIV
+// console.log(convertToRoman(798));  // DCCXCVIII
+// console.log(convertToRoman(36));   // XXXVI
 
 // do not edit below this line
 module.exports = convertToRoman
