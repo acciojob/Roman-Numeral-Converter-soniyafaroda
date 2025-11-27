afaaaa595a8c64fed0b6160055294d0bb0a21481
Soniya Faroda <143260442+soniyafaroda@users.noreply.github.com>
2025-11-27 17:17:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,35 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  if (num === 0) return ""; // Roman numerals have no zero
 
-  //your code here
+  const symbols = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1]
+  ];
 
-}
-  let roman = "";
+  let result = "";
 
-  // Loop through the object values
-  for (let i = 0; i <= 6; i++) {
-    const symbol = obj[i][0];
-    const value = obj[i][1];
-
-    // Add the symbol while num is greater than or equal to value
+  for (let [roman, value] of symbols) {
     while (num >= value) {
-      roman += symbol;
+      result += roman;
       num -= value;
     }
   }
 
-  // Handle subtractive notation replacements
-  roman = roman
-    .replace(/DCCCC/g, "CM") // 900
-    .replace(/CCCC/g, "CD")  // 400
-    .replace(/LXXXX/g, "XC") // 90
-    .replace(/XXXX/g, "XL")  // 40
-    .replace(/VIIII/g, "IX") // 9
-    .replace(/IIII/g, "IV"); // 4
-
-  return roman;
+  return result;
 }
 
-// Example tests:
+// Example tests
 console.log(convertToRoman(14));   // XIV
 console.log(convertToRoman(798));  // DCCXCVIII
-
-// do not edit below this line
-module.exports = convertToRoman
+console.log(convertToRoman(100000)); // (100 × M)
